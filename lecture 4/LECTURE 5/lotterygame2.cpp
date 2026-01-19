213bf0f16cffacb0e2f7b48d247ceaf18728c1ee
Nikhil9131 <nikhilrathore10b@gmail.com>
2026-01-19 14:35:10 +0530

    pattern7

---
+++ lecture 4/LECTURE 5/lotterygame2.cpp
@@ -0,0 +1,47 @@
+    #include<iostream>
+    using namespace std;
+    int main() {
+        int n;
+        cin>> n;
+        if (n>=300 && n<=460){
+            cout<<"you won a Macbook"<<endl;
+            if(n>=300 && n<=380){
+            cout<<"Model: M1 Macbook"<<endl;
+        }else {
+            cout<<"Model: M2 Macbook"<<endl;
+        }
+
+        } 
+        else if ( n>=200 && n<=280){
+            cout<<"you won a pack of Kurkure"<<endl;
+            if(n>=200 && n<=240){
+            cout<<"Flavour:chili kurkure"<<endl;
+        }
+        else {
+            cout<<"Flavour: onion kurkure"<<endl;
+        }
+        } 
+
+        else if (n>=1100 && n<=1500){
+            cout<<"you won a cycle"<<endl;
+            if(n>=1100 && n<=1300){
+            cout<<"Brand: Avon cycle"<<endl;
+        }
+        else {
+            cout<<"Brand: Hero Cycle"<<endl;
+        }
+        } 
+        else if (n>=50 && n<=80){
+            cout<<"you won a Bike"<<endl;
+            if(n>=50 && n<=65){
+            cout<<"Model: Bullet"<<endl;
+        }
+        else {
+            cout<<"Model: Rajdoot"<<endl;
+        }
+        } 
+        else{
+            cout<<"Better luck next time"<<endl;
+        }
+        return 0;
+    }
