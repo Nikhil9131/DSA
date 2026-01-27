005b0c394fc605d63e603d7ff7642e5d058b13fe
Nikhil9131 <nikhilrathore10b@gmail.com>
2026-01-27 16:04:38 +0530

    test

--- lecture 4/pattern23.cpp
+++ lecture 4/pattern23.cpp
@@ -3,10 +3,13 @@ using namespace std ;
 int main () {
     int n ;
     cin>>n ;
-    for (int i=0; i<n; i++) {
-        for (j=1;j<=2*i-1;j++){
-            if (j%2==0){
-                cout<<"* "
+    for (int i=1; i<=n; i++) {
+        for (j=1;j<=i;j++){
+            if (j%2!=0){
+                cout<< i <<" ";
+            }else{ 
+                cout<<2*n-i<< " ";
+            }
             }
         }
 
