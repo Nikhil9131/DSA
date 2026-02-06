dd8761ae11ec111bc5327b9183f3e267c0c64f67
Nikhil9131 <nikhilrathore10b@gmail.com>
2026-02-06 13:21:37 +0530

    prime

--- lecture 15/1_fibbonacci_series.cpp
+++ lecture 15/1_fibbonacci_series.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
 int fibonacci(int n){
-    if(n==0 or n==1){
-        return n;
+        if(n==0 or n==1){
+            return n;
+        }
+        return fibonacci(n-1) + fibonacci(n-2);
     }
-    return fibonacci(n-1) + fibonacci(n-2);
-}
-int main() {
-    int n;
-    cin>>n;
-    cout<<fibonacci(n)<<endl;
-    return 0;
-}
+    int main() {
+        int n;
+        cin>>n;
+        cout<<fibonacci(n)<<endl;
+        return 0;
+    }
