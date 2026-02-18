f46174f6440925e05437af1e0d119a1c246b99cb
Nikhil9131 <nikhilrathore10b@gmail.com>
2026-02-18 17:15:34 +0530

    bubble sort

---
+++ leetcode/2_happy_number.cpp
@@ -0,0 +1,47 @@
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin>> n;
+//     while (n!=1){
+//         int sum =0;
+
+//             while(n>0){
+//                 int r=n%10;
+//                 sum+= r*r;
+//                 n/= 10;
+//             }
+//             sum=n;
+
+//             }
+//             return true;
+
+//     }
+
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int sum=0;
+//     int n;
+//     cin>> n;
+//     bool isHappy(int n) 
+//     if(n==1 or n==7) {
+//         return true;
+
+//     }else if (n<10){
+//         return false;
+
+//     }else{
+//         int sum =0;
+
+//             while(n!=0){
+//                 int r=n%10;
+//                 sum+= r*r;
+//                 n=n/10;
+//             }
+//             return isHappy(sum);
+//     }
+
+//     }
