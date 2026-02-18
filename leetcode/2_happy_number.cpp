27eb160965374aa1ce4cf45bea72b819333582c3
Nikhil9131 <nikhilrathore10b@gmail.com>
2026-02-18 17:53:27 +0530

    happy number

--- leetcode/2_happy_number.cpp
+++ leetcode/2_happy_number.cpp
@@ -18,30 +18,41 @@
 //             return true;
 
 //     }
+                                    //  Leetcode-202 Happy Number //
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int main() {
-//     int sum=0;
-//     int n;
-//     cin>> n;
-//     bool isHappy(int n) 
-//     if(n==1 or n==7) {
-//         return true;
+// Recursive function to check Happy Number
+bool isHappy(int n) {
+    if(n == 1 || n == 7) {
+        return true;
+    }
+    else if(n < 10) {
+        return false;
+    }
+    else {
+        int sum = 0;
+        while(n != 0) {
+            int r = n % 10;
+            sum += r * r;
+            n = n / 10;
+        }
+        return isHappy(sum); // recursion call
+    }
+}
 
-//     }else if (n<10){
-//         return false;
+int main() {
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
 
-//     }else{
-//         int sum =0;
+    if(isHappy(n))
+        cout << "Happy Number";
+    else
+        cout << "Not Happy Number";
 
-//             while(n!=0){
-//                 int r=n%10;
-//                 sum+= r*r;
-//                 n=n/10;
-//             }
-//             return isHappy(sum);
-//     }
+    return 0;
+}
 
-//     }
+  
