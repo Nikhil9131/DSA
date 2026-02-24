76d20191294695410db5070324ff389488b44fb4
Nikhil9131 <nikhilrathore10b@gmail.com>
2026-02-24 22:34:14 +0530

    66

---
+++ leetcode/10_66.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+vector<int> plusOne(vector<int>& digits) {
+        for(int i=digits.size()-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+int main() {
+    vector<int> digits={1,2,3};
+    vector<int> a=plusOne(digits);
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
