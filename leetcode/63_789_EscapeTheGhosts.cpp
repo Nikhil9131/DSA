740cb23dfb70dec653a2c989816a7f5ed2135541
Nikhil9131 <nikhilrathore10b@gmail.com>
2026-06-17 11:11:06 +0530

    Escape The Ghosts

---
+++ leetcode/63_789_EscapeTheGhosts.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool escapeGhosts(vector<vector<int>>& ghosts, vector<int>& target) {
+        int xapna =target[0];
+        int yapna =target[1];
+        int dist =abs(yapna)+abs(xapna);
+        for(auto i: ghosts){
+            int d=abs(xapna-i[0])+abs(yapna-i[1]);
+
+            if(d<=dist){
+                return false;
+            }
+        }
+        return true;
+    }
+};
