3158f82bd0c058644cbc8c0d5ed2589be585405c
Your Name <nikhilrathore10b@gmail.com>
2026-08-07 23:48:56 +0530

    3280

--- leetcode/89_3280_ConvertDatetoBinary.cpp
+++ leetcode/89_3280_ConvertDatetoBinary.cpp
@@ -17,16 +17,13 @@ public:
         reverse(res.begin(), res.end());
         return res;
     }
-
     string convertDateToBinary(string date) {
         int year = stoi(date.substr(0, 4));
         int month = stoi(date.substr(5, 2));
         int day = stoi(date.substr(8, 2));
-
         return toBinary(year) + "-" + toBinary(month) + "-" + toBinary(day);
     }
 };
-
 int main() {
     Solution obj;
     string date;
