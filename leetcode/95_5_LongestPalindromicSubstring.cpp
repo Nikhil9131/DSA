744cee6eb0f9cb7435f365dd6fc22ed627a69ddb
Your Name <nikhilrathore10b@gmail.com>
2026-08-11 21:44:50 +0530

    5

---
+++ leetcode/95_5_LongestPalindromicSubstring.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int start = 0;
+        int maxLen = 1;
+
+        for (int i = 0; i < s.size(); i++) {
+
+            // -------------------------
+            // Odd length palindrome
+            // -------------------------
+            int left = i;
+            int right = i;
+
+            while (left >= 0 && right < s.size() &&
+                   s[left] == s[right]) {
+
+                if (right - left + 1 > maxLen) {
+                    start = left;
+                    maxLen = right - left + 1;
+                }
+
+                left--;
+                right++;
+            }
+
+            // -------------------------
+            // Even length palindrome
+            // -------------------------
+            left = i;
+            right = i + 1;
+
+            while (left >= 0 && right < s.size() &&
+                   s[left] == s[right]) {
+
+                if (right - left + 1 > maxLen) {
+                    start = left;
+                    maxLen = right - left + 1;
+                }
+
+                left--;
+                right++;
+            }
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
+
+int main() {
+    Solution obj;
+
+    string s;
+    cin >> s;
+
+    cout << obj.longestPalindrome(s);
+
+    return 0;
+}
