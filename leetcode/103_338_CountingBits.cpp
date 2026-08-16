bf240083171da2ffe5bea95a13e913f857e13b18
Your Name <nikhilrathore10b@gmail.com>
2026-08-16 23:35:05 +0530

    338

---
+++ leetcode/103_338_CountingBits.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+
+        vector<int> ans(n + 1, 0);
+
+        for (int i = 1; i <= n; i++) {
+
+            ans[i] = ans[i & (i - 1)] + 1;
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    int n;
+    cin >> n;
+
+    vector<int> ans = obj.countBits(n);
+
+    for (int i = 0; i <= n; i++) {
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}
