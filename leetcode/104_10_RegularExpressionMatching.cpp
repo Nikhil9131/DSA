8b730be84d0cf583f276b2f03a99ede5c6a4ec96
Your Name <nikhilrathore10b@gmail.com>
2026-08-19 23:42:06 +0530

    10

---
+++ leetcode/104_10_RegularExpressionMatching.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+bool isMatch(string s, string p) {
+    int n = s.length();
+    int m = p.length();
+
+    vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+
+    dp[0][0] = true;
+
+    // Handle patterns like a*, a*b*, a*b*c*
+    for (int j = 2; j <= m; j++) {
+        if (p[j - 1] == '*') {
+            dp[0][j] = dp[0][j - 2];
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+
+            // Normal character or '.'
+            if (p[j - 1] == s[i - 1] || p[j - 1] == '.') {
+                dp[i][j] = dp[i - 1][j - 1];
+            }
+
+            // '*'
+            else if (p[j - 1] == '*') {
+
+                // '*' matches zero characters
+                dp[i][j] = dp[i][j - 2];
+
+                // '*' matches one or more characters
+                if (p[j - 2] == '.' || p[j - 2] == s[i - 1]) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j];
+                }
+            }
+        }
+    }
+
+    return dp[n][m];
+}
+
+int main() {
+
+    string s, p;
+
+    cout << "Enter string: ";
+    cin >> s;
+
+    cout << "Enter pattern: ";
+    cin >> p;
+
+    if (isMatch(s, p))
+        cout << "true" << endl;
+    else
+        cout << "false" << endl;
+
+    return 0;
+}
