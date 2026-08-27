3c102d7dabdf1151fe832be7b2b40577f9276b17
Your Name <nikhilrathore10b@gmail.com>
2026-08-27 21:36:11 +0530

    56

---
+++ leetcode/105_56_MergeIntervals.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    sort(intervals.begin(), intervals.end());
+
+    vector<vector<int>> ans;
+
+    for (auto interval : intervals) {
+        if (ans.empty() || ans.back()[1] < interval[0]) {
+            ans.push_back(interval);
+        } 
+        else {
+            ans.back()[1] = max(ans.back()[1], interval[1]);
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<vector<int>> intervals = {
+        {1, 3},
+        {2, 6},
+        {8, 10},
+        {15, 18}
+    };
+
+    vector<vector<int>> result = merge(intervals);
+
+    for (auto interval : result) {
+        cout << "[" << interval[0] << ", " << interval[1] << "] ";
+    }
+
+    return 0;
+}
